356da9a562d76350a7d17ef6501c4b7715b6e72a
Your Namecls <you@example.com>
2026-08-08 00:19:41 +0530

    day14_updates

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+ 
+function NotFound() {
+return (
+<div style={{ textAlign: 'center', padding: '100px' }}>
+{'404 Page Not FoundBack to Home'}
+</div>
+);
+}
+export default NotFound;
+ 
