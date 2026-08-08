9a18072178345a1635fb8ec38c2f8941ff98326a
Your Namecls <you@example.com>
2026-08-08 21:22:53 +0530

    day14 updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // src/App.jsx
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
